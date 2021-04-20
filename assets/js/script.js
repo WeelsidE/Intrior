47e1136c60442e332e8000d680a24f8069232030
WeelsidE <69400112+WeelsidE@users.noreply.github.com>
2021-04-20 14:13:25 +0700

    ver. 1.4

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,17 @@
 'use strict'
 
+document.querySelector('#btn').addEventListener('click', function (e) {
+    let modal = document.querySelector('.modal');
+
+    modal.classList.add('_active');
+
+    e.preventDefault();
+});
+
+document.querySelector('#icon_close').addEventListener('click', function (e) {
+    let modal = document.querySelector('.modal');
+
+    modal.classList.remove('_active');
+
+    e.preventDefault();
+});
