648e302d4c69990fcc7a15b518171d36fec59be0
WeelsidE <69400112+WeelsidE@users.noreply.github.com>
2021-04-28 17:38:52 +0700

    ver. 2.0

--- assets/js/script.js
+++ assets/js/script.js
@@ -326,3 +326,30 @@ projectBtns[3].addEventListener('mouseover', function () {
         projectItems[2].classList.remove('_active');
     });
 });
+
+
+
+
+
+
+//Анимация блоков ===============================================================================
+AOS . init ({
+    // Global settings:
+    disable: 'mobile',
+    startEvent: 'DOMContentLoaded',
+    initClassName: 'aos-init',
+    animatedClassName: 'aos-animate',
+    useClassNames: false,
+    disableMutationObserver: false,
+    debounceDelay: 50,
+    throttleDelay: 99,
+
+    // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+    offset: 175,
+    delay: 0,
+    duration: 400,
+    easing: 'ease',
+    once: true,
+    mirror: false,
+    anchorPlacement: 'top-bottom',
+});
